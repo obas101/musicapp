8406f11af25b073e3d1e0121f5e86fbf3ba748bb
Yunusa Onivehu <obadobay@gmail.com>
2020-11-02 17:56:22 +0100

    new

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,18 @@
 	<div id="app">
 		<div class="main">
 			<div>
-				<div class="header"><span class="headtext">MusicApp</span></div>
+				<div class="header"><span class="headtext">Music</span></div>
+			</div>
+			<div class="large-spacing"></div>
+
+			<div class="title">
+				<span class="title">Music Title</span>
+			</div>
+			<div class="small-spacing"></div>
+			<div class="control">
+				<button id="small-button">Prev</button>
+				<button id="large-button">Play</button>
+				<button id="small-button">Next</button>
 			</div>
 		</div>
 	</div>
@@ -17,6 +28,7 @@ export default {
 
 <style>
 * {
+	font-family: Segoe UI;
 	padding: 0;
 	margin: 0;
 }
@@ -24,14 +36,52 @@ export default {
 	height: 100vh;
 }
 .header {
-	width: 100%;
 	background: black;
 	color: white;
+	text-align: center;
 	padding: 1.5rem;
-  text-align: center;
+}
+.control {
+	display: flex;
+	justify-content: center;
+}
+.large-spacing {
+	margin-top: 4rem;
+	width: 100%;
+}
+.small-spacing {
+	margin-top: 2rem;
+	width: 100%;
+}
+.title {
+	font-size: 3rem;
+	text-align: center;
+	font-weight: 500;
 }
 .headtext {
 	font-size: 2.5rem;
 	font-weight: 700;
+	letter-spacing: 6px;
+}
+button {
+	margin-left: 1.5rem;
+	margin-right: 1.5rem;
+	border-radius: 8px;
+	outline: none;
+	border-color: thistle;
+}
+#small-button {
+	background: #ff4587;
+	color: white;
+	font-size: 14px;
+	font-weight: 700;
+	padding: 0.4rem 1.2rem 0.4rem 1.2rem !important;
+}
+#large-button {
+	background: #ff0076;
+	color: white;
+	font-size: 20px;
+	font-weight: 700;
+	padding: 0.8rem 1.8rem 0.8rem 1.8rem !important;
 }
 </style>
